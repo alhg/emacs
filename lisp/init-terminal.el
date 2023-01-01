591786bb1bacbe27ccf6afff3d114468e1d78a64
alhg <alhg@users.noreply.github.com>
2023-01-01 11:53:50 -0800

    Add init-terminal (eat) and fix filename

---
+++ lisp/init-terminal.el
@@ -0,0 +1,11 @@
+(straight-use-package
+ '(eat :type git
+       :host codeberg
+       :repo "akib/emacs-eat"
+       :files ("*.el" ("term" "term/*.el") "*.texi"
+               "*.ti" ("terminfo/e" "terminfo/e/*")
+               ("terminfo/65" "terminfo/65/*")
+               ("integration" "integration/*")
+               (:exclude ".dir-locals.el" "*-tests.el"))))
+
+(provide 'init-terminal)
