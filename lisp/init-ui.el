f9c05f6b121216cd6f9c7f5f625fee0ddb3e5361
alhg <alhg@users.noreply.github.com>
2023-03-28 21:02:43 -0700

    Remove global-hl-line, add hl-line-mode to text, prog modes

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -5,7 +5,10 @@
 (setq-default column-number-mode t) ;; display column number beside line number on modeline
 (display-time)                      ;; display time on modeline
 
-(global-hl-line-mode +1)       ;; positive turns on global highlight line
+;; (global-hl-line-mode +1)       ;; positive turns on global highlight line
+(add-hook 'text-mode-hook 'hl-line-mode)
+(add-hook 'prog-mode-hook 'hl-line-mode)
+
 (setq blink-cursor-interval 0) ;; 0 stops blinking cursor
 (setq x-stretch-cursor t)      ;; stretch cursor to fit width of character. Useful for detecting tabs.
 
