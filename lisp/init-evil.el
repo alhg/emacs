cf58a56036928678d4234e11d117bc3e86c4d11a
alhg <alhg@users.norepy.github.com>
2026-02-08 13:19:05 -0800

    Fix loading evil config in emacs 30

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,14 +1,20 @@
+;;; init-evil.el --- EVIL Configuration -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
 (use-package evil
   :config
   ;; disable vim keybindings in insert mode
-  ;;(setq evil-disable-insert-state-bindings t)
+  (setq evil-disable-insert-state-bindings t)
   ;;(evil-mode +1)
   )
 
-;; (use-package evil-collection
-;;   :after evil
-;;   :config
-;;   (setq evil-want-integration t)
-;;   (evil-collection-init))
+(use-package evil-collection
+  :after evil
+  :config
+  (setq evil-want-integration t)
+  (evil-collection-init))
 
 (provide 'init-evil)
